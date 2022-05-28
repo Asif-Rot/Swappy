803c12c9df0dbada2d456c48b2b9b3b8088afa97
daniel231195 <dansela123@gmail.com>
2022-05-28 19:19:05 +0300

    signin

--- client/src/routes/SignIn.js
+++ client/src/routes/SignIn.js
@@ -14,7 +14,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
-import Home from './Home';
+// import Home from './Home';
 import {Route, useHistory} from 'react-router-dom';
 import App from '../App'
 import logo from '../swapy.jpeg'
