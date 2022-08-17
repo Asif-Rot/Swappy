9b8256161d95ed3629dbb7e66d6bb88112526052
Asif-Rot <74010095+Asif-Rot@users.noreply.github.com>
2022-08-17 14:43:39 +0300

    changes in home (center all items)

--- client/src/components/BestBook.js
+++ client/src/components/BestBook.js
@@ -1,23 +1,13 @@
-import {useSpringCarousel} from 'react-spring-carousel'
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import {styled} from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
-import ListItem from '@mui/material/ListItem';
-import Stack from '@mui/material/Stack';
 import React, {useContext, useEffect, useState} from "react";
 import {UserContext} from "../context/userContext";
-import CircularProgress from '@mui/material/CircularProgress';
-import Backdrop from '@mui/material/Backdrop';
 import {useHistory} from 'react-router-dom';
 import Button from "@mui/material/Button";
 import { Typography } from '@mui/material';
 
 export default function SpringCarousel() {
-    const [imgBook, setImgBook] = React.useState('');
-    const [isLoading, setIsLoading] = useState(false);
     const [items, setItems] = useState([]);
     const [favorites, setFavorites] = useState([]);
     const history=useHistory();
@@ -40,7 +30,7 @@ export default function SpringCarousel() {
     }
 
     const getType = (type) => {
-        if(type == "book")
+        if(type === "book")
             return "ספר"
         else
             return "משחק וידיאו"
@@ -83,10 +73,12 @@ export default function SpringCarousel() {
 
     return (
         <div>
-        <Grid container> 
-            <Box style={{marginRight: 80, marginBlockEnd: -80, marginTop: 50,}}>
+            <Box display="flex"
+                 alignItems="center"
+                 justifyContent="center">
                 <h2>הפריטים החדשים באתר :</h2>
             </Box>
+        <Grid container justifyContent="center">
             <Box
                 sx={{
                     display: 'flex',
@@ -126,45 +118,44 @@ export default function SpringCarousel() {
                 The best five book in web
              */}
 
-            <Box style={{marginRight: 80, marginBlockEnd: -80, marginTop: 50}}>
+            <Box display="flex"
+                 alignItems="center"
+                 justifyContent="center">
                 <h2>המומלצים ביותר עבורך :</h2>
             </Box>
-            <Grid container>
-        <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: {md: 'row'},
-                    alignItems: 'center',
-                    bgcolor: 'background.paper',
-                    // bgcolor:  'black',
-                    overflow: 'auto',
-                    borderRadius: '16px',
-                    boxShadow: 15,
-                    fontWeight: 'bold',
-                    m: 10,
-                    justifyContent: 'flex-start',
-                    alignContent: 'flex-start',
-
-                }}
-            >
-                {favorites.length?
-                    favorites.map((item, i) => (
-                <Grid item mx={1} key={i}>                
-                    <Card sx={{display: "flex",flexDirection: "column", boxShadow:2, height:400}}>
-                    <Typography  variant="inherit" textAlign={'center'} maxHeight={25} marginTop={1.5}>{item.name}</Typography>
-                        <Button onClick={handleDetails.bind(this,item._id)} sx={{height:320}}>
-                        <img src={item.image}
-                             alt='item_img'
-                             width={200}
-                        />
-                        </Button>
-                        <Typography   textAlign={'center'} > <b><u>קטגוריה</u>:</b> {getType(item.item_type)}</Typography>
-                    </Card>
-                </Grid>
-                    )) : <p><br/>אין פריטים עדיין...</p>}
-            </Box>
-        </Grid>
+            <Grid container justifyContent="center">
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: {md: 'row'},
+                        alignItems: 'center',
+                        bgcolor: 'background.paper',
+                        overflow: 'auto',
+                        borderRadius: '16px',
+                        boxShadow: 15,
+                        fontWeight: 'bold',
+                        m: 10,
+                        justifyContent: 'flex-start',
+                        alignContent: 'flex-start',
+                    }}
+                >
+                    {favorites.length?
+                        favorites.map((item, i) => (
+                            <Grid item mx={1} key={i}>
+                                <Card sx={{display: "flex",flexDirection: "column", boxShadow:2, height:400}}>
+                                    <Typography  variant="inherit" textAlign={'center'} maxHeight={25} marginTop={1.5}>{item.name}</Typography>
+                                    <Button onClick={handleDetails.bind(this,item._id)} sx={{height:320}}>
+                                        <img src={item.image}
+                                             alt='item_img'
+                                             width={200}
+                                        />
+                                    </Button>
+                                    <Typography   textAlign={'center'} > <b><u>קטגוריה</u>:</b> {getType(item.item_type)}</Typography>
+                                </Card>
+                            </Grid>
+                        )) : <p><br/>אין פריטים עדיין...</p>}
+                </Box>
+            </Grid>
         </div>
-
     );
 }
